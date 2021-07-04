41d4049f3cee1cec1f44292c4ff24418a16c7479
Pablo Molleda <pmolledadelrio@gmail.com>
2021-07-04 13:07:01 +0200

    updated

--- JP and Co/models/New-hire.model.js
+++ JP and Co/models/New-hire.model.js
@@ -1,23 +1,45 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const userSchema = new Schema(
   {
-    username: { type: String, unique: true },
-    name: String,
-    password: String,
-    profileImg: String,
-    description: String,
-    role: {
-      type: String,
-      enum: ['PM', 'DEV', 'TA', 'STUDENT'],
-      default: 'STUDENT'
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    contract: {
+      type: {
+        type: String,
+        enum: ['permanent', 'fixed-term', 'casual', 'zero-hour'],
+        required: true
+      },
+      duration: { type: String },
+      positionTitle: { type: String, required: true }
+    },
+    employee: {
+      name: { type: String, required: true },
+      surname: { type: String, required: true },
+      address: {
+        street: { type: String, required: true },
+        buildingNumber: { type: String },
+        zipCode: { type: String, required: true },
+        city: { type: String, required: true },
+        country: { type: String, required: true }
+      },
+      phone: { type: String },
+      personalId: { type: String, required: true },
+      NIN: { type: String, required: true }
+    },
+    agreementDetails: {
+      startDate: { type: Date, required: true },
+      endDate: { type: Date, required: true },
+      weeklyHours: { type: Number, required: true },
+      yearlyBonus: { type: Number, required: true },
+      functions: { type: String },
+      trialPeriodDuration: { type: Number },
+      location: { type: String, default: 'remote' },
+      signDate: { type: Date, default: Date.now, required: true }
     }
   },
-  {
-    timestamps: true
-  }
-);
+  { timestamps: true }
+)
 
 const User = mongoose.model('User', userSchema);
 
--- JP and Co/models/Selling-vehicle.model.js
+++ JP and Co/models/Selling-vehicle.model.js
@@ -1,24 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    username: { type: String, unique: true },
-    name: String,
-    password: String,
-    profileImg: String,
-    description: String,
-    role: {
-      type: String,
-      enum: ['PM', 'DEV', 'TA', 'STUDENT'],
-      default: 'STUDENT'
+const sellingCarSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  purchasePrice: { type: Number, required: true },
+  vehicleYear: { type: Date, required: true },
+  vehicleStatus: {
+    type: String,
+    enum: ['new', 'used', 'broken'],
+    required: true
+  },
+  vehicleModel: { type: String, required: true },
+  vehiclePlate: { type: String, required: true },
+  conditions: { type: String },
+  buyer: {
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    personalId: { type: String, required: true },
+    address: {
+      street: { type: String, required: true },
+      buildingNumber: { type: String },
+      zipCode: { type: String, required: true },
+      city: { type: String, required: true },
+      country: { type: String, required: true }
     }
   },
-  {
-    timestamps: true
-  }
-);
+  agrementDate: { type: Date, required: true },
+  signDate: { type: Date, required: true }
+},
+  { timestamps: true }
+)
 
-const User = mongoose.model('User', userSchema);
+const sellingCar = mongoose.model('sellingCar', sellingCarSchema)
 
-module.exports = User;
+module.exports = sellingCar
