beb100f8490a24718c250c66338ff9ab0ad982b6
Pablo Molleda <pmolledadelrio@gmail.com>
2021-07-04 17:15:55 +0200

    done

---
+++ routes/base.routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router()
+
+//pagina principal
+
+router.get("/", (req, res) => res.render("index"))
+
+// router.get("/my-profile", (req, res) => res.render("index"))
+
+// router.get("/my-profile/edit", (req, res) => res.render("index"))
+
+// router.get("/book", (req, res) => res.render("index"))
+
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-// module.exports = app => {
-//   app.use('/', require('./base.routes.js'))
+module.exports = app => {
+  app.use('/', require('./base.routes.js'))
 //   app.use('/services', require('./services.routes.js'))
 //   app.use('/services/legal/vehicle-agreement-selling', require('./selling-vehicle.routes.js'))
 //   app.use('/services/legal/vehicle-agreement-buying', require('./buying-vehicle.routes.js'))
@@ -8,4 +8,4 @@
 //   app.use('/services/business/invoice', require('./invoice.routes.js'))
 //   app.use('/', require('./auth.routes.js'))
 //   app.use('/user', require('./user.routes.js'))
-// }
+}
